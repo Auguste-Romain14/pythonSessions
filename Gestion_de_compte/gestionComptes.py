059ae505f1a50194c9411d50999b3fb73d2eef6a
Ako Lath Auguste <te.lauguste@gmail.com>
2024-05-10 12:50:20 +0000

    push part 2

--- Gestion_de_compte/gestionComptes.py
+++ Gestion_de_compte/gestionComptes.py
@@ -1,14 +1,18 @@
 class CompteBancaire:
+  list_compte_bancaire = []
+
   def __init__(self, numero_compte:int, nom:str, solde:float):
     self.numero_compte: int = numero_compte
     self.nom: str= nom
     self.__solde: float = solde
+    CompteBancaire.list_compte_bancaire.append(self)
+
 
   def versement(self, montant:float):
     self.__solde += montant
 
   def afficher_info(self):
-    print(f"Non: {self.nom}, Numero de compte: {self.numero_compte}, Solde: {self.__solde}")
+    print(f"Nom: {self.nom}, Numero de compte: {self.numero_compte}, Solde: {self.__solde}")
 
   def retrait(self, montant:float):
     if(montant <= self.__solde):
@@ -20,9 +24,21 @@ class CompteBancaire:
     taxe = (5 * self.__solde) /100
     self.__solde -= taxe
 
+  @classmethod
+  def ajout_compte(cls, compte):
+    cls.list_compte_bancaire.append(compte)
+
+  @classmethod
+  def afficher_comptes(cls):
+    for compte in cls.list_compte_bancaire:
+      print(f"Nom: {compte.nom}, Numero de compte: {compte.numero_compte}, Solde: {compte.__solde}")
+
+
 
 
 client1 = CompteBancaire(1, "Kouadio", 10000 )
+client2 = CompteBancaire(35, "Yao", 5000 )
+client3 = CompteBancaire(25, "Konan", 26800 )
 
 client1.afficher_info()
 client1.versement(2500.59)
@@ -32,3 +48,5 @@ client1.afficher_info()
 client1.retrait(15000)
 client1.agios()
 client1.afficher_info()
+print("********************")
+CompteBancaire.afficher_comptes()
